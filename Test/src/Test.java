3e32884e9533620f1681a6b987689ca54aeb6d83
omkar-devloper <166119920+omkar-devloper@users.noreply.github.com>
2024-06-30 18:00:37 +0530

    c
    cmt by omkar

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -1,7 +1,7 @@
 
 public class Test {
 	public static void main(String[] a) {
-		System.out.println("Hi welcome to java 30 june 2024");
+		System.out.println("Hi welcome to java 30 june 2024 part2");
 
 	}
 }
